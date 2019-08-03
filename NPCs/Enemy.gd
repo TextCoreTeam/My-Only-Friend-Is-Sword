8fb61359fbe67635962350e2c86a3dbe24b37d20
Hitonoriol <gorgilike@mail.ru>
2019-08-03 08:49:12 +0300

    Mob aim kludge

--- NPCs/Enemy.gd
+++ NPCs/Enemy.gd
@@ -8,6 +8,7 @@ var can_take_dmg = true
 func _on_takedmg_timeout():
 	can_take_dmg = true
 	$TakeDMGTimer.stop()
+	$Blood.emitting = false
 
 func _ready():
 	$Blood.emitting = false
@@ -25,7 +26,7 @@ func _on_shoot_timeout():
 	if (!detected):
 		return
 	var bullet_speed = 6
-	var direction = Vector2(cos(get_rotation()), sin(get_rotation()))
+	var direction = Vector2(cos($Aim.get_rotation()), sin($Aim.get_rotation()))
 	var spawn_distance = 70
 	var spawn_point = get_global_position() + direction * spawn_distance
 	var bullet = bullet_s.instance()
@@ -33,7 +34,7 @@ func _on_shoot_timeout():
 	bullet.get_node("Bullet_area/Sprite").frame = 0
 	world.add_child(bullet)
 	bullet.set_global_position(spawn_point)
-	bullet.get_node('Bullet_area').velocity = (Vector2(cos(get_rotation()) * bullet_speed, sin(get_rotation()) * bullet_speed))
+	bullet.get_node('Bullet_area').velocity = (Vector2(cos($Aim.get_rotation()) * bullet_speed, sin($Aim.get_rotation()) * bullet_speed))
 	pass
 
 func _on_timer_timeout():
@@ -43,7 +44,6 @@ func _on_timer_timeout():
 func mob():	#kludge for mob identification because im a f4g
 	pass
 
-var blood_s = load("res://Particles/Blood.tscn")
 func dmg():
 	if (can_take_dmg):
 		print("Mob took damage")
@@ -82,10 +82,10 @@ func _physics_process(delta):
 		detected = false
 	
 	if (detected):
-		if get_angle_to(player.global_position) > 0:
-    		rotation += turn_speed
+		if $Aim.get_angle_to(player.global_position) > 0:
+    		$Aim.rotation += turn_speed
 		else:
-    		rotation -= turn_speed
+    		$Aim.rotation -= turn_speed
 		collision = move_and_collide(dir * speed * delta)
 		if (collision && can_attack && collision.get_collider().has_method("pdmg")):
 			collision.collider.dmg()
