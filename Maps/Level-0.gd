fe8335dfe4cd355d9877e085bd7b9324ef4cde41
BitterCakeEater <valur010@gmail.com>
2019-08-04 21:13:53 +0300

    Tutorial

--- Maps/Level-0.gd
+++ Maps/Level-0.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 func _ready():
 	var level = get_parent()
-	level.name_str = "Shit from the ass"
+	level.name_str = ""
 	level.update_map_name()
+	level.show_msg("Press LMB to throw the sword; Press LMB to bring your sword back; As you can see, you can move just by throwing your sword; Try to not to fall down; Be aware of enemies! Good luck")
 	pass
 	
 func update_score(points):
